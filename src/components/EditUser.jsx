40a6e3f1823ec0cd3124d9b1a27842091ab767e6
GONZOLOL <glerdie489@g.educaand.es>
2023-03-20 10:14:36 +0100

    edit user ended and styled

--- src/components/EditUser.jsx
+++ src/components/EditUser.jsx
@@ -3,25 +3,34 @@ import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { getLocalStorageToken } from "../utils/LocalStorage";
 import { getLocalStorageId } from "../utils/LocalStorage";
+import { ReactComponent as Save } from "../media/save.svg";
+import { ReactComponent as Cross } from "../media/cross.svg";
 
 export function EditUser() {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState({
+    name: "",
+    surname: "",
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState("");
+
   const id = getLocalStorageId();
   const navigate = useNavigate();
 
   useEffect(() => {
     const token = getLocalStorageToken("token");
 
-    fetch(`http://51.38.51.187:5050/api/v1/users/${getLocalStorageId()}`, {
+    fetch(`${import.meta.env.VITE_APP_API_LINK}/users/${getLocalStorageId()}`, {
       headers: { Authorization: `Bearer ${token}` },
     })
       .then((data) => data.json())
       .then((data) => {
         setData(data);
       });
-  }, []);
+  }, [id, getLocalStorageToken]);
 
-  const sendEdit = (event) => {
+  function submit(event) {
     event.preventDefault();
 
     const name = event.target.name.value;
@@ -30,7 +39,7 @@ export function EditUser() {
     const password = event.target.password.value;
 
     if (name && surname && email && password) {
-      fetch(`http://51.38.51.187:5050/api/v1/users/${id}`, {
+      fetch(`${import.meta.env.VITE_APP_API_LINK}/users/${id}`, {
         headers: {
           Authorization: `Bearer ${getLocalStorageToken()}`,
           "Content-Type": "application/json",
@@ -57,9 +66,7 @@ export function EditUser() {
           }
         });
     }
-  };
-
-  console.log(data.email);
+  }
 
   return (
     <section className="mainBox">
@@ -69,32 +76,38 @@ export function EditUser() {
             <Link to="/users" className="back">
               <Back />
             </Link>
-            <span className="tittle">{data.name} Profile</span>
+            <span className="tittle">{data.name}</span>
           </div>
-          <form className="userContent" onSubmit={sendEdit}>
+          <form onSubmit={submit} className="userContent">
             <div className="content">
               <div className="contentContainer">
                 <span className="tag">Name</span>
                 <input
                   type="text"
                   className="info"
-                  placeholder={data.name ? data.name : "No data"}
+                  defaultValue={data ? data.name : "No data"}
+                  required
+                  id="name"
                 />
               </div>
               <div className="contentContainer">
                 <span className="tag">Surname</span>
                 <input
-                  type="text"
+                  type="tex"
                   className="info"
-                  placeholder={data.surname ? data.surname : "No data"}
+                  defaultValue={data ? data.surname : "No data"}
+                  required
+                  id="surname"
                 />
               </div>
               <div className="contentContainer">
                 <span className="tag">Mail</span>
                 <input
-                  type="text"
+                  type="email"
                   className="info"
-                  placeholder={data.email ? data.email : "No data"}
+                  defaultValue={data ? data.email : "No data"}
+                  required
+                  id="email"
                 />
               </div>
               <div className="contentContainer">
@@ -102,23 +115,26 @@ export function EditUser() {
                 <input
                   type="text"
                   className="info"
-                  placeholder={data.password ? data.password : "No data"}
+                  placeholder="Update your password"
+                  required
+                  id="password"
                 />
               </div>
             </div>
             <div className="iconButtons">
-              <div className="editButton" type="submit">
-                <div>
-                  <span>Save</span>
-                </div>
+              <div className="saveButton">
+                <button type="Submit">Save</button>
+                <Save />
               </div>
-              <div className="deleteButton">
+              <div className="cancelButton">
                 <Link to="/users" className="deleteLink">
                   <span>Cancel</span>
+                  <Cross />
                 </Link>
               </div>
             </div>
           </form>
+          {error ? <div className="errorMessage">{error}</div> : ""}
         </div>
       </div>
     </section>
